c7b8c8a80470799b30dba382543e4d644941e815
Paul Panarese <git@panjunction.com>
2011-05-10 16:41:45 -0700

    Fixed some whitespace, and replaced color with either random generator or a fixed schema in the options. Made background color non-white for contrast in test and CSS.

--- public/javascripts/jquery.covisuals.js
+++ public/javascripts/jquery.covisuals.js
@@ -7,8 +7,8 @@
 //(function(B){B.color={};B.color.make=function(F,E,C,D){var G={};G.r=F||0;G.g=E||0;G.b=C||0;G.a=D!=null?D:1;G.add=function(J,I){for(var H=0;H<J.length;++H){G[J.charAt(H)]+=I}return G.normalize()};G.scale=function(J,I){for(var H=0;H<J.length;++H){G[J.charAt(H)]*=I}return G.normalize()};G.toString=function(){if(G.a>=1){return"rgb("+[G.r,G.g,G.b].join(",")+")"}else{return"rgba("+[G.r,G.g,G.b,G.a].join(",")+")"}};G.normalize=function(){function H(J,K,I){return K<J?J:(K>I?I:K)}G.r=H(0,parseInt(G.r),255);G.g=H(0,parseInt(G.g),255);G.b=H(0,parseInt(G.b),255);G.a=H(0,G.a,1);return G};G.clone=function(){return B.color.make(G.r,G.b,G.g,G.a)};return G.normalize()};B.color.extract=function(D,C){var E;do{E=D.css(C).toLowerCase();if(E!=""&&E!="transparent"){break}D=D.parent()}while(!B.nodeName(D.get(0),"body"));if(E=="rgba(0, 0, 0, 0)"){E="transparent"}return B.color.parse(E)};B.color.parse=function(F){var E,C=B.color.make;if(E=/rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(F)){return C(parseInt(E[1],10),parseInt(E[2],10),parseInt(E[3],10))}if(E=/rgba\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]+(?:\.[0-9]+)?)\s*\)/.exec(F)){return C(parseInt(E[1],10),parseInt(E[2],10),parseInt(E[3],10),parseFloat(E[4]))}if(E=/rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(F)){return C(parseFloat(E[1])*2.55,parseFloat(E[2])*2.55,parseFloat(E[3])*2.55)}if(E=/rgba\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\s*\)/.exec(F)){return C(parseFloat(E[1])*2.55,parseFloat(E[2])*2.55,parseFloat(E[3])*2.55,parseFloat(E[4]))}if(E=/#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(F)){return C(parseInt(E[1],16),parseInt(E[2],16),parseInt(E[3],16))}if(E=/#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(F)){return C(parseInt(E[1]+E[1],16),parseInt(E[2]+E[2],16),parseInt(E[3]+E[3],16))}var D=B.trim(F).toLowerCase();if(D=="transparent"){return C(255,255,255,0)}else{E=A[D]||[0,0,0];return C(E[0],E[1],E[2])}};var A={aqua:[0,255,255],azure:[240,255,255],beige:[245,245,220],black:[0,0,0],blue:[0,0,255],brown:[165,42,42],cyan:[0,255,255],darkblue:[0,0,139],darkcyan:[0,139,139],darkgrey:[169,169,169],darkgreen:[0,100,0],darkkhaki:[189,183,107],darkmagenta:[139,0,139],darkolivegreen:[85,107,47],darkorange:[255,140,0],darkorchid:[153,50,204],darkred:[139,0,0],darksalmon:[233,150,122],darkviolet:[148,0,211],fuchsia:[255,0,255],gold:[255,215,0],green:[0,128,0],indigo:[75,0,130],khaki:[240,230,140],lightblue:[173,216,230],lightcyan:[224,255,255],lightgreen:[144,238,144],lightgrey:[211,211,211],lightpink:[255,182,193],lightyellow:[255,255,224],lime:[0,255,0],magenta:[255,0,255],maroon:[128,0,0],navy:[0,0,128],olive:[128,128,0],orange:[255,165,0],pink:[255,192,203],purple:[128,0,128],violet:[128,0,128],red:[255,0,0],silver:[192,192,192],white:[255,255,255],yellow:[255,255,0]}})(jQuery);
 
 
-(function($) {
-	$.fn.timeline = function(type, data_, options_) {
+(function ($) {
+	$.fn.timeline = function (type, data_, options_) {
 		// data_ should be formatted as {"video":{"duration":"hh:mm:ss"},"data":{"series_1_name": [{"start_time": "hh:mm:ss", "duration": "hh:mm:ss"}]}}
 		var ctx;
 		var target = this;
@@ -21,25 +21,25 @@
 		var point_options = {};
 		var line_options = {};
 		var autoscale = false;
-    var options;
-    var data;
+		var options;
+		var data;
 		try {
 			options = JSON.parse(options_);
-		} catch(err) {
+		} catch (err) {
 			options = options_;
 		}
 		try {
 			data = JSON.parse(data_);
-		} catch(error) {
+		} catch (error) {
 			data = data_;
 		}
-		
+
 		//console.warn("Type:" + type + ",Data:" + data_ + ",Options:" + options_) // DEBUGGING INPUT
-		
-		if (this[0].tagName == "CANVAS") {
+
+		if (this[0].tagName === "CANVAS") {
 			ctx = this[0].getContext("2d");
 			// Parses input options (see function below)
-			parseOptions(options,true);
+			parseOptions(options, true);
 			// fillGenerator(Object.keys(data["data"]).length); // Uncomment to activate colored lines
 			// Height and width of target if set. Else, 500px X 100px is default. Can also be set in options.
 			if (this.css('width')) {
@@ -48,45 +48,50 @@
 			if (this.css('height')) {
 				c_height = parseInt(this.css('height'), 10);
 			}
-			
-			this.attr('width',c_width).attr('height',c_height);
-			
+
+			this.attr('width', c_width).attr('height', c_height);
+
 			if (Object.keys(data.data).length > 0 && Object.keys(data.video).length > 0) {
 				// Vertical Scaling
 				autoscale = true; // BUG (autoscale set to false in parseOptions)
 				if (autoscale === true) {
-					c_height = Object.keys(data.data).length*15+20;
-					this.css('height',c_height.toString() + "px");
-					this.attr('height',c_height);
+					c_height = Object.keys(data.data).length * 15 + 20;
+					this.css('height', c_height.toString() + "px");
+					this.attr('height', c_height);
 				}
-				
+
+				// Generate Colors
+				var colorschema = options.colorschema || {};
+				fillGenerator(Object.keys(data.data).length,colorschema);
+
 				var scale_ratio = scaler(data.video);
-				if (type == "line") {
-					vector(100,0,100,c_height,{});
-					line_display(data,options,scale_ratio);
-				} else if (type == "frequency") {
-					frequency_display(data,options,scale_ratio);
+				if (type === "line") {
+					vector(100, 0, 100, c_height, {});
+					text("t->",102,c_height-12,8,'black',{})
+					line_display(data, options, scale_ratio);
+				} else if (type === "frequency") {
+					frequency_display(data, options, scale_ratio);
 				} else {
 					console.error('CoVisual Plot Type not recognized. See Documentation for Details');
 				}
 			} else {
 				console.error('No Data. See Documentation for JSON formatting');
 			}
-						
-		} else if (this[0].tagName == "DIV") {
+
+		} else if (this[0].tagName === "DIV") {
 				options.css = {};
 				if (!options.css.width) {
-					options.css.width = this.css('width'); + "\"";
+					options.css.width = this.css('width');
 				}
 				if (!options.css.height) {
-					options.css.height = this.css('height'); + "\"";
+					options.css.height = this.css('height');
 				}
-				
+
 				// preserve border
 				if (!options.css.border) {
 					options.css.border = this[0].style.border;
 				}
-				
+
 				this.replaceWith("<canvas id=\"covisual_" + (document.getElementsByTagName('canvas').length + 1).toString() + "\"><!-- --></canvas>");
 				json_options_ = JSON.stringify(options);
 				var json_data_ = JSON.stringify(data);
@@ -95,7 +100,7 @@
 		} else {
 				console.error("CoVisual TimeLines require HTML5 Canvas Support. Target must be a div or canvas.");
 		}
-		
+
 		function line_display(line_data_,line_options_,scale_data_) {
 			var i=1;
 			$.each(data["data"], function (series_name,series_data) {
@@ -132,11 +137,11 @@
 				i++;
 			});
 		}
-		
+
 		function frequency_display(freq_data, freq_options,scale_data_) {
-			
+
 		}
-		
+
 		// Scaler
 		function scaler(video_data_) {
 			data_width = parseInt(c_width,10) - 100;
@@ -154,19 +159,20 @@
 			spp = seconds/data_width; // seconds per pixel
 			return spp;
 		}
-		
+
 		// Line Subfunction. line_options_ is a JSON object
 		function line(start_x,length,series_number,line_options_) {
 			ctx.lineWidth = 3;
+			ctx.strokeStyle = color_list[series_number - 1];
 			parseOptions(line_options_,false);
 			ctx.beginPath();
-			y = series_number*15 + 4; 
+			y = series_number * 15 + 4; 
 			ctx.moveTo(start_x, y);
 			ctx.lineTo(start_x + length, y);
 			ctx.stroke();
 			ctx.closePath();
 		}
-		
+
 		function vector(start_x,start_y,end_x,end_y,line_options_) {
 			ctx.lineWidth = 1;
 			parseOptions(line_options_,false);
@@ -176,7 +182,7 @@
 			ctx.stroke();
 			ctx.closePath();
 		}
-		
+
 		function point(x,y,series_number,point_options_) {
 			var radius = 2;
 			parseOptions(point_options_,false);
@@ -185,10 +191,11 @@
 			ctx.closePath();
 			ctx.fill();
 		}
-		
+
 		function label(labeltext, series_number, label_options_) {
 			ctx.textBaseline = 'top';
 			ctx.textAlign = 'left';
+			ctx.fillStyle = color_list[series_number - 1];
 			ctx.font = "12pt Arial";
 			maxWidth = 100;
 			parseOptions(label_options_,false);
@@ -197,6 +204,16 @@
 			ctx.fillText(labeltext,4, y,maxWidth);
 		}
 		
+		function text(labeltext,x,y,size,color,label_options_) {
+			ctx.textBaseline = 'top';
+			ctx.textAlign = 'left';
+			ctx.fillStyle = color || 'black';
+			ctx.font = parseInt(size,10).toString() + "pt Arial";
+			maxWidth = 100;
+			parseOptions(label_options_,false);
+			ctx.fillText(labeltext,x, y,maxWidth);
+		}
+
 		// Option Parsing function. Escapes most javascript functions
 		function parseOptions(opts,global_flag) {
 			$.each(opts, function(i,val) {
@@ -239,40 +256,22 @@
 				}
 			});
 		}
-		
+
 		// Fill Generator from FLOT (see https://github.com/flot/flot)
 		// Needs manipulation for adding new colors to existing colors and for more than 3 colors
-		function fillGenerator(number) {
+		function fillGenerator(number,schema) {
 			var colors = [], variation = 0;
 			i = 0;
-			needColors = number - color_list.length;
+			var neededColors = number - color_list.length;
 			while (colors.length < neededColors) {
-			    var c;
-			    if (color_list.length == i){ // check degenerate case
-			        c = $.color.make(100, 100, 100);
-          }
-			    else{
-			        c = $.color.parse(color_list[i]);
-          }
-            
-			    // vary color if needed
-			    var sign = variation % 2 == 1 ? -1 : 1;
-			    c.scale('rgb', 1 + sign * Math.ceil(variation / 2) * 0.2);
-            
-			    // FIXME: if we're getting to close to something else,
-			    // we should probably skip this one
-			    colors.push(c);
-            
-			    ++i;
-			    if (i >= color_list.length) {
-			        i = 0;
-			        ++variation;
-			    }
+				var color = schema[i] || '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+				colors.push(color);
+				i++;
 			}
 			$.each(colors, function(obj) {
 				color_list.push(colors[obj].toString());
 			});
 		}
-		
+
 	};
 })(jQuery);
